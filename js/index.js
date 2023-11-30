bfca5703e2018e5e3a79d589131315a05b1be2bf
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2023-11-29 23:55:47 -0300

    falta só o animation

--- js/index.js
+++ js/index.js
@@ -1,26 +1,53 @@
 let Game = {
-    elementos: {
-        localcards: document.getElementById("cards")
-    },
-    valores: {
-        emojis: [
-            "😀",
-            "😁",
-            "😎",
-            "🥰",
-            "😴",
-            "🤑",
-            "😭",
-            "😨"
-        ],
-        cardsabertos: []
-    }
-}
+  elementos: {
+    localcards: document.getElementById("cards")
+  },
+  valores: {
+    emojis: [
+      "😀","😁","😎","🥰","😴","🤑","😭","😨","😀","😁","😎","🥰","😴","🤑","😭","😨"
+    ],
+    cardsabertos: [],
+    cardsabertoshtml: [],
+    emojisEmbaralhados: "",
+  },
+};
+
+Game.valores.emojisEmbaralhados = Game.valores.emojis.sort(() =>
+  Math.random() > 0.5 ? 2 : -1
+);
+
 function criarCards() {
-    for (let i = 0; i < 16; i++) {
-        let element = document.createElement("div");
-        Game.elementos.localcards.appendChild(element)
-        element.className = `card`
-    }
+  for (let i = 0; i < 16; i++) {
+    let element = document.createElement("div");
+
+    Game.elementos.localcards.appendChild(element);
+
+    element.className = `card`;
+
+    element.addEventListener("click", () => {
+      element.innerHTML = `${Game.valores.emojisEmbaralhados[i]}`;
+      element.id = `${Game.valores.emojisEmbaralhados[i]}`;
+
+      Game.valores.cardsabertos.push(element.id);
+      Game.valores.cardsabertoshtml.push(element);
+
+      if (Game.valores.cardsabertos.length == 2) {
+        if (Game.valores.cardsabertos[0] == Game.valores.cardsabertos[1]) {
+          Game.valores.cardsabertos = [];
+          Game.valores.cardsabertoshtml = [];
+
+        } else {
+          Game.valores.cardsabertoshtml[0].innerHTML = ``;
+          Game.valores.cardsabertoshtml[1].innerHTML = ``;
+
+          Game.valores.cardsabertos = [];
+          Game.valores.cardsabertoshtml = [];
+        }
+      }
+    });
+  }
 }
-criarCards()
+criarCards();
+function reset() {
+    window.location.reload()
+}
